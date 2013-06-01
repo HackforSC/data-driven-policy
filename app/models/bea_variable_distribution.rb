96e6e575ab4cbf7afd760a2c1e370b3f460bcc22
brycesenz <bryce.senz@gmail.com>
2013-06-01 13:59:02 -0400

    Changing migration files

--- app/models/bea_variable_distribution.rb
+++ app/models/bea_variable_distribution.rb
@@ -1,28 +1,26 @@
 # == Schema Information
 #
-# Table name: distributions
+# Table name: bea_variable_distributions
 #
 #  id                                     :integer      not null, primary key
 #  key_code                               :string       not null
-#  msa                                    :string       not null
 #  year                                   :integer      not null
 #  mean                                   :decimal      not null
 #  std_dev                                :decimal      not null
 #  created_at                             :datetime     not null
 #  updated_at                             :datetime     not null
 
-class Distribution < ActiveRecord::Base  
+class BeaVariableDistribution < ActiveRecord::Base  
   # Associations
   #----------------------------------------------------------------------------
 
   # Accessible Methods
   #----------------------------------------------------------------------------
-  attr_accessible :key_code, :msa, :year, :mean, :std_dev
+  attr_accessible :key_code, :year, :mean, :std_dev
 
   # Validations
   #----------------------------------------------------------------------------
   validates :key_code, :presence => true
-  validates :msa, :presence => true
   validates :year, :presence => true
   validates :mean, :presence => true
   validates :std_dev, :presence => true
