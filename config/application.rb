484c378d63fe26b51c09248a464a5d3206d18dbf
brycesenz <bryce.senz@gmail.com>
2013-06-26 21:53:57 -0400

    Changing configuration

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DataDrivenPolicy
     # -- all .rb files in that directory are automatically loaded.
 
     # For Heroku
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
